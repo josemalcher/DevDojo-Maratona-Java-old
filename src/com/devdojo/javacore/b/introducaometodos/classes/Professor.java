693678109c85e70f2ab6d165a53662aaac780306
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-10-16 03:42:35 -0300

    Aula 31: Métodos com parâmetros tipo reference pt 01 -> AJUSTE no parametro de recebimento do método! (para ficar igual ao video da aula!)

--- src/com/devdojo/javacore/b/introducaometodos/classes/Professor.java
+++ src/com/devdojo/javacore/b/introducaometodos/classes/Professor.java
@@ -9,9 +9,9 @@ public class Professor {
 
     public void imprime(Professor aux) {
         System.out.println("----------------------");
-        System.out.println(this.cpf);
-        System.out.println(this.matricula);
-        System.out.println(this.nome);
-        System.out.println(this.rg);
+        System.out.println(aux.cpf);
+        System.out.println(aux.matricula);
+        System.out.println(aux.nome);
+        System.out.println(aux.rg);
     }
 }
